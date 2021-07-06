4bef6e6a525510cdb33ba5472b5d4afd22081f2e
ChunYS <ajuys95@ajou.ac.kr>
2021-07-06 22:55:31 +0900

    Test

---
+++ AlgorithmStudy/21Y07M/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cin >> a; 
+    cout << a << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    return 0;
+}
