55e1004255d89e8e2c81aa28a8c533dc01dd81a7
ChunYS <ajuys95@ajou.ac.kr>
2021-08-31 16:33:32 +0900

    Create 117272nTiling2.py

---
+++ 알고리즘문제/21Y08M/117272nTiling2.py
@@ -0,0 +1,14 @@
+import sys
+read = sys.stdin.readline
+write = sys.stdout.write
+
+n = int(read())
+
+dp = [1 for _ in range(n+1)]
+dp[2] = 3
+
+for i in range(3, n+1):
+    dp[i] = (dp[i-1] + dp[i-2]*2) % 10007
+
+print(dp[n])
+
