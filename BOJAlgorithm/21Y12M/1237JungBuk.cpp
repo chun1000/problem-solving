0f568489bf245be52f7e99bad0c1e16f7dc14e4f
ChunYS <60660485+ajchun15@users.noreply.github.com>
2021-12-14 23:40:03 +0900

    update

---
+++ BOJAlgorithm/21Y12M/1237JungBuk.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <ios>
+using namespace std;
+
+void use_boj_io() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+//백준 전용 입출력 속도 개선
+
+int main()
+{
+	use_boj_io();
+	cout << "문제의 정답";
+
+
+	return 0;
+}
+
